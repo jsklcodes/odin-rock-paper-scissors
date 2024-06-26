deb57e107d717342916cf43b411dc260e8ef38e2
Kallyne Layane <hellookallyne@gmail.com>
2024-06-26 19:42:38 -0300

    Comment code before refactoring

--- index.js
+++ index.js
@@ -1,66 +1,66 @@
-const getComputerChoice = () => {
-  const choices = ['rock', 'paper', 'scissors']
-  const randomIndex = Math.floor(Math.random() * 3)
+// const getComputerChoice = () => {
+//   const choices = ['rock', 'paper', 'scissors']
+//   const randomIndex = Math.floor(Math.random() * 3)
 
-  return choices[randomIndex]
-}
+//   return choices[randomIndex]
+// }
 
-const getHumanChoice = () => {
-  let humanChoice = prompt('Rock, paper or scissors?')
+// const getHumanChoice = () => {
+//   let humanChoice = prompt('Rock, paper or scissors?')
 
-  return humanChoice.toLowerCase()
-}
+//   return humanChoice.toLowerCase()
+// }
 
-const playGame = () => {
-  let humanScore = 0
-  let computerScore = 0
-  let numberOfRounds = 0
-  let numberOfDraws = 0
+// const playGame = () => {
+//   let humanScore = 0
+//   let computerScore = 0
+//   let numberOfRounds = 0
+//   let numberOfDraws = 0
 
-  const playRound = (humanChoice, computerChoice) => {
-    console.log(`Round #${numberOfRounds + 1}`)
+//   const playRound = (humanChoice, computerChoice) => {
+//     console.log(`Round #${numberOfRounds + 1}`)
 
-    if (humanChoice === computerChoice) {
-      console.log('Draw!')
-      numberOfDraws++
-    } else if (
-      (humanChoice === 'paper' && computerChoice === 'rock') ||
-      (humanChoice === 'rock' && computerChoice === 'scissors') ||
-      (humanChoice === 'scissors' && computerChoice === 'paper')
-    ) {
-      console.log('You won the round!')
-      humanScore++
-    } else {
-      console.log('Computer won the round!')
-      computerScore++
-    }
+//     if (humanChoice === computerChoice) {
+//       console.log('Draw!')
+//       numberOfDraws++
+//     } else if (
+//       (humanChoice === 'paper' && computerChoice === 'rock') ||
+//       (humanChoice === 'rock' && computerChoice === 'scissors') ||
+//       (humanChoice === 'scissors' && computerChoice === 'paper')
+//     ) {
+//       console.log('You won the round!')
+//       humanScore++
+//     } else {
+//       console.log('Computer won the round!')
+//       computerScore++
+//     }
 
-    console.log(
-      `You chose ${humanChoice} and Computer chose ${computerChoice}.`
-    )
-    console.log(
-      `You: ${humanScore} - Computer: ${computerScore} - Draw: ${numberOfDraws}`
-    )
-  }
+//     console.log(
+//       `You chose ${humanChoice} and Computer chose ${computerChoice}.`
+//     )
+//     console.log(
+//       `You: ${humanScore} - Computer: ${computerScore} - Draw: ${numberOfDraws}`
+//     )
+//   }
 
-  while (numberOfRounds < 5) {
-    const humanChoice = getHumanChoice()
-    const computerChoice = getComputerChoice()
+//   while (numberOfRounds < 5) {
+//     const humanChoice = getHumanChoice()
+//     const computerChoice = getComputerChoice()
 
-    playRound(humanChoice, computerChoice)
-    numberOfRounds++
-  }
+//     playRound(humanChoice, computerChoice)
+//     numberOfRounds++
+//   }
 
-  if (numberOfRounds === 5) {
-    if (humanScore === computerScore) {
-      console.log('The game ended in a draw!')
-    } else if (humanScore > computerScore) {
-      console.log('You won the game!')
-    } else {
-      console.log('Computer won the game!')
-    }
-  }
+//   if (numberOfRounds === 5) {
+//     if (humanScore === computerScore) {
+//       console.log('The game ended in a draw!')
+//     } else if (humanScore > computerScore) {
+//       console.log('You won the game!')
+//     } else {
+//       console.log('Computer won the game!')
+//     }
+//   }
 
-  console.log('Final score:')
-  console.log(`You: ${humanScore} x Computer: ${computerScore}`)
-}
+//   console.log('Final score:')
+//   console.log(`You: ${humanScore} x Computer: ${computerScore}`)
+// }
