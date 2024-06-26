14afb3109a3fdc7b05daa32876a71172400d1cd5
Kallyne Layane <hellookallyne@gmail.com>
2024-06-26 10:46:15 -0300

    Add function to play game

--- index.js
+++ index.js
@@ -10,3 +10,57 @@ const getHumanChoice = () => {
 
   return humanChoice.toLowerCase()
 }
+
+const playGame = () => {
+  let humanScore = 0
+  let computerScore = 0
+  let numberOfRounds = 0
+  let numberOfDraws = 0
+
+  const playRound = (humanChoice, computerChoice) => {
+    console.log(`Round #${numberOfRounds + 1}`)
+
+    if (humanChoice === computerChoice) {
+      console.log('Draw!')
+      numberOfDraws++
+    } else if (
+      (humanChoice === 'paper' && computerChoice === 'rock') ||
+      (humanChoice === 'rock' && computerChoice === 'scissors') ||
+      (humanChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+      console.log('You won the round!')
+      humanScore++
+    } else {
+      console.log('Computer won the round!')
+      computerScore++
+    }
+
+    console.log(
+      `You chose ${humanChoice} and Computer chose ${computerChoice}.`
+    )
+    console.log(
+      `You: ${humanScore} - Computer: ${computerScore} - Draw: ${numberOfDraws}`
+    )
+  }
+
+  while (numberOfRounds < 5) {
+    const humanChoice = getHumanChoice()
+    const computerChoice = getComputerChoice()
+
+    playRound(humanChoice, computerChoice)
+    numberOfRounds++
+  }
+
+  if (numberOfRounds === 5) {
+    if (humanScore === computerScore) {
+      console.log('The game ended in a draw!')
+    } else if (humanScore > computerScore) {
+      console.log('You won the game!')
+    } else {
+      console.log('Computer won the game!')
+    }
+  }
+
+  console.log('Final score:')
+  console.log(`You: ${humanScore} x Computer: ${computerScore}`)
+}
